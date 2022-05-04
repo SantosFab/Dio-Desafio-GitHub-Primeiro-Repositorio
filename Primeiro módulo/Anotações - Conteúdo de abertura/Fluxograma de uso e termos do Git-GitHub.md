d2aea5aea668909fbf9391e6bdd9a0776f034daf
SantosFab <fabriciosantos.dev@gmail.com>
2022-05-04 16:59:01 -0300

    atualização-segundo módulo

---
+++ Primeiro módulo/Anotações - Conteúdo de abertura/Fluxograma de uso e termos do Git-GitHub.md
@@ -0,0 +1,55 @@
+# Criar chave - ssh
+
+1. Gere um Git Bash no desktop
+2. Digite o código ssh-keygen -t ed25519 -C fabriciosantos.dev@gmail.com
+3. Elabore/coloque uma senha
+4. Acesse a pasta, pelo Git Bash, com o endereço c/users/fabri(**usuário do pc**)/ .ssh (.**ssh é uma pasta oculta**)
+5. Digite dir, dentro da pasta ssh, para visualizar as chaves.
+6. cat id_ed25519.pub – expor a chave (pública)
+7. Copiar a chave, logo após, vá até o site e registre a chave no GitHub
+8. Retorne ao Git Bash, coloque o código: eval $(ssh-agent -s)
+9. Logo após, o código - ssh-add id_ed25519 – expor chave (privada), posteriormente, o sistema vai pedirá a senha anteriormente digitada ou criada.
+
+# Vincular um repositório virtual a uma pasta local
+
+1.   Vá no site do GitHub, pegue o endereço do HTTPS/SSH no repositório
+2.   Digite, no Git Bash: git clone (espaço) e copie o endereço da etapa anterior 
+3.   Digite "Yes"
+4.   git init = iniciar um repositório 
+5.   git config --global user.email "fabriciosantos.dev@gmail.com"  - caso necessário
+6.   git config --global user.name “SantosFab” - caso necessário
+7.   Após efetuar alterações de arquivos
+8.   git add * = adicionar todos os arquivos no commit
+9.   git commit -m “**comentário**” = preparar o documento para upload - commitar
+10.   git push origin main
+
+# Vincular o commit local a um repositório
+
+1. Executar o Git Bash na pasta do commit desejada
+2. git init = iniciar um repositório 
+3. git config --global user.email "fabriciosantos.dev@gmail.com" - caso necessário
+4. git config --global user.name “SantosFab” - caso necessário
+5. git remote add **origin** (espaço) e o link do novo repositório (site/HTTPS/SSH) =  vincular ao repositório virtual (executar essa atividade somente na primeira vez)
+6. git push origin master = enviar para o repositório remoto 
+7. git pull origin master = puxar o arquivo do repositório virtual, antes de enviar - caso necessário
+8. corrigir possível alterações
+9. git add * = adicionar todos os arquivos no commit
+10. git push origin master
+#  Termos
+
+* dir -a = visualizar pastas ocultas
+
+* mv nome do arquivo ./nome da pasta = mover arquivo para outra pasta
+
+* git config --list = lista de todas as configurações no seu git
+
+* git config -–global –-unset user.email = apagar email
+
+* git config –-global –-unset user.name = apagar name
+
+* pwd = mostrar todos os caminhos até chegar ao local do endereço atual
+
+* git remote -v = lista de repositório remoto que você tem cadastrado
+
+* git status = pendência do repositório (máquina)
+
---
+++ Primeiro módulo/Site elaborado após aula sobre HTML e CSS/Primeiro site - Teste.php
@@ -0,0 +1,84 @@
+<DOCTYPE html>
+<html>
+<head>
+<title> Portfólio - Fabrício</title> 
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+<header>
+<h1 id="title">Uso do GitHub <img class="img"src="GitHub.png"alt="Imagem da logo do GitHub"</h1>
+</header>
+<article class="post">
+<section>
+<h2 class="subtitle"> Criar chave - SSH</h2>
+<ol>
+<li>Gere um Git Bash no desktop
+<li>Digite o código ssh-keygen -t ed25519 -C "<a href="mailto:fabriciosantos.dev@gmail.com"target="_blank">fabriciosantos.dev@gmail.com</a>"
+<li>Elabore/coloque uma senha
+<li>Acesse a pasta, pelo Git Bash, com o endereço c/users/fabri(usuário do pc)/ .ssh (.**ssh é uma pasta oculta**)
+<li>Digite dir, dentro da pasta ssh, para visualizar as chaves.
+<li>cat id_ed25519.pub – expor a chave (pública)
+<li>Copiar a chave, logo após, vá até o site e registre a chave no GitHub
+<li>Retorne ao Git Bash, coloque o código: eval $(ssh-agent -s)
+<li>Logo após, o código - ssh-add id_ed25519 – expor chave (privada), posteriormente, o sistema vai pedirá a senha anteriormente digitada ou criada.
+</ol>
+</section>
+<section>
+<h2 class="subtitle">Vincular o commit local a um repositório</h2>
+<ol>
+<li>Executar o Git Bash na pasta do commit desejada
+<li>git init = iniciar um repositório 
+<li>git config --global user.email "<a href="mailto:fabriciosantos.dev@gmail.com"target="_blank">fabriciosantos.dev@gmail.com</a>" - caso necessário
+<li>git config --global user.name “<a href="https://github.com/SantosFab"target="_blank">SantosFab</a>" - caso necessário
+<li>git remote add **origin** (espaço) e o link do novo repositório (site/HTTPS/SSH) =  vincular ao repositório virtual (executar essa atividade somente na primeira vez)
+<li>git push origin master = enviar para o repositório remoto 
+<li>git pull origin master = puxar o arquivo do repositório virtual, antes de enviar - caso necessário
+<li>corrigir possível alterações
+<li>git add * = adicionar todos os arquivos no commit
+<li>git push origin master
+</ol>
+</section>
+<section>
+<h2 class="subtitle">Vincular um repositório virtual a uma pasta local</h2>
+<ol>
+<li>Vá no site do GitHub, pegue o endereço do HTTPS/SSH no repositório
+<li>Digite, no Git Bash: git clone (espaço) e copie o endereço da etapa anterior 
+<li>Digite "Yes"
+<li>git init = iniciar um repositório 
+<li>git config --global user.email "<a href="mailto:fabriciosantos.dev@gmail.com"target="_blank">fabriciosantos.dev@gmail.com</a>"  - caso necessário
+<li>git config --global user.name “<a href="https://github.com/SantosFab"target="_blank">SantosFab</a>" - caso necessário
+<li>Após efetuar alterações de arquivos
+<li>git add * = adicionar todos os arquivos no commit
+<li>git commit -m “**comentário**” = preparar o documento para upload - commitar
+<li>git push origin main
+</ol>
+</section>
+<section>
+<h2 class="subtitle">Termos</h2>
+<ul>
+<li>dir -a = visualizar pastas ocultas
+<li>mv nome do arquivo ./nome da pasta = mover arquivo para outra pasta
+<li>git config --list = lista de todas as configurações no seu git
+<li>git config -–global –-unset user.email = apagar email
+<li>git config –-global –-unset user.name = apagar name
+<li>pwd = mostrar todos os caminhos até chegar ao local do endereço atual
+<li>git remote -v = lista de repositório remoto que você tem cadastrado
+<li>git status = pendência do repositório (máquina)
+</ul>
+</section>
+<section>
+</section>
+</article>
+</body>
+<footer =class="qq">
+<p>
+<a href="mailto:fabriciosantos.dev@gmail.com"target="_blank">E-mail para contato</a>
+</p>
+<p>
+<a href="https://www.linkedin.com/in/fabr%C3%ADcio-santos-915a79237/"target="_blank">LinkedIn</a> 
+</p>
+<p>
+<a href="https://github.com/SantosFab"target="_blank">GitHub</a>
+</p>
+</footer>
+</html>
